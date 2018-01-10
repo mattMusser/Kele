3a33cb65256eb83f6ef6d08432c814c9377fdc29
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-10 13:45:53 -0600

    Roadmaps and Checkpoints finished, added correct checkpoint id

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -6,7 +6,7 @@ module Roadmap
 	end
 
 	def get_checkpoint(checkpoint_id)
-		response = self.class.get(api_url("checkpoints/157"), headers: { "authorization" => @auth_token })
+		response = self.class.get(api_url("checkpoints/2553"), headers: { "authorization" => @auth_token })
 
 		@checkpoint = JSON.parse(response.body)
 	end
