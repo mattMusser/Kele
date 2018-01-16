1ccddf2bbc675ca553cceb7a806e5d606de1813a
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-16 16:12:24 -0600

    fix for revision

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,7 @@ class Kele
 	include HTTParty
 
 	def initialize(email, password)
-		response = self.class.post(api_url, body: { email: email, password: password })
+		response = self.class.post(api_url("sessions"), body: { email: email, password: password })
 
 		if response.code == 200
 			@auth_token = response["auth_token"]
@@ -15,26 +15,18 @@ class Kele
 	end
 
 	def get_me
-		response = self.class.get(me_url, headers: { "authorization" => @auth_token })
+		response = self.class.get(api_url("user/me"), headers: { "authorization" => @auth_token })
 		JSON.parse(response.body)
 	end
 
 	def get_mentor_availability(mentor_id)
-		response = self.class.get(mentor_url("#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })
+		response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
 		JSON.parse(response.body)git
 	end
 
 	private
 
 	def api_url
-		"https://www.bloc.io/api/v1/sessions"
-	end
-
-	def me_url
-		"https://www.bloc.io/api/v1/users/me"
-	end
-
-	def mentor_url(endpoint)
-		"https://www.bloc.io/api/v1/mentors/#{endpoint}"
+		"https://www.bloc.io/api/v1/#{ endpoint }"
 	end
 end
