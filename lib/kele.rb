723a8cf3e1c64eb4ea1074565eff16b346719d27
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-17 10:36:59 -0600

    Fix errors and spacing in kele.rb

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,7 +5,7 @@ class Kele
 	include HTTParty
 
 	def initialize(email, password)
-		response = self.class.post(api_url("sessions"), body: { email: email, password: password })
+		response = self.class.post(api_url("sessions"), body: {email: email, password: password})
 
 		if response.code == 200
 			@auth_token = response["auth_token"]
@@ -15,18 +15,18 @@ class Kele
 	end
 
 	def get_me
-		response = self.class.get(api_url("user/me"), headers: { "authorization" => @auth_token })
+		response = self.class.get(api_url("users/me"), headers: {"authorization" => @auth_token})
 		JSON.parse(response.body)
 	end
 
 	def get_mentor_availability(mentor_id)
-		response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
-		JSON.parse(response.body)git
+		response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token})
+		JSON.parse(response.body)
 	end
 
 	private
 
-	def api_url
-		"https://www.bloc.io/api/v1/#{ endpoint }"
+	def api_url(endpoint)
+		"https://www.bloc.io/api/v1/#{endpoint}"
 	end
 end
