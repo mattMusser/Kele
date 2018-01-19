c592cb1def33c69725b515b0beb91a28a0b13da0
mattMusser <webdevelopmentlyspeaking@gmail.com>
2018-01-19 14:51:18 -0600

    give each object own line

--- lib/kele.rb
+++ lib/kele.rb
@@ -7,7 +7,8 @@ class Kele
 	include Roadmap
 
 	def initialize(email, password)
-		response = self.class.post(api_url("sessions"), body: {email: email, password: password})
+		response = self.class.post(api_url("sessions"),
+																			 body: {email: email, password: password})
 
 		if response.code == 200
 			@auth_token = response["auth_token"]
@@ -18,13 +19,15 @@ class Kele
 	end
 
 	def get_me
-		response = self.class.get(api_url("users/me"), headers: {"authorization" => @auth_token})
+		response = self.class.get(api_url("users/me"),
+																			headers: {"authorization" => @auth_token})
 
 		@user = JSON.parse(response.body)
 	end
 
 	def get_mentor_availability(mentor_id)
-		response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: {"authorization" => @auth_token})
+		response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"),
+																			headers: {"authorization" => @auth_token})
 
 		@availability = JSON.parse(response.body)
 	end
@@ -38,13 +41,24 @@ class Kele
 	end
 
 	def create_message(sender_email, recipient_id, message_body, subject)
-		response = self.class.post(api_url("messages"), headers: { "authorization" => @auth_token }, body: {sender: sender_email, recipient_id: recipient_id, stripped_text: message_body, subject: subject})
+		response = self.class.post(api_url("messages"),
+																			 headers: { "authorization" => @auth_token },
+																			 body: {sender: sender_email,
+																			 				recipient_id: recipient_id,
+																			 				stripped_text: message_body,
+																			 				subject: subject})
 
 		puts "Your message has been sent!" if response.success?
 	end
 
 	def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
-		response = self.class.post(api_url("checkpoint_submissions"), headers: headers, body: {checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: enrollment_id})
+		response = self.class.post(api_url("checkpoint_submissions"),
+																			 headers: headers,
+																			 body: {checkpoint_id: checkpoint_id,
+																			 				assignment_branch: assignment_branch,
+																			 				assignment_commit_link: assignment_commit_link,
+																			 				comment: comment,
+																			 				enrollment_id: enrollment_id})
 
 		puts "Checkpoint has been submitted" if response.success?
 	end
